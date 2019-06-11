71a616d0e0a93fbf692033c35e41fad81bea8e97
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-06-11 19:50:59 +0300

    Add files via upload

---
+++ HW0506_09/IsraelTime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HW0506_09
+{
+    class IsraelTime
+    {
+        private static IsraelTime _instance;
+        string timeIL;
+        private static object key = new object();
+        private IsraelTime()
+        {
+            timeIL = DateTime.Now.ToString();
+        }
+        public static void GetTime()
+        {
+            if (_instance == null)
+            {
+                lock (key)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new IsraelTime();
+                    }
+                }
+            }
+            Console.WriteLine($"Time in Israel is {_instance.timeIL}");           
+        }
+
+    }
+}
